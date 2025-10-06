e7e764c0f35395d5226bd910f74c357cd97c8164
subzero1221 <158075799+subzero1221@users.noreply.github.com>
2025-10-06 21:33:15 +0400

    Update server.js

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: process.env.NODE_ENV === "production",
+      secure: true,
       httpOnly: true,
       sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
