779cc0957ded54408e90cb99f7c4bbfd700e0f3b
subzero1221 <158075799+subzero1221@users.noreply.github.com>
2025-10-06 19:48:09 +0400

    Update socket.js

--- socket.js
+++ socket.js
@@ -9,7 +9,7 @@ let io;
 const initSocket = (server) => {
   io = new Server(server, {
     cors: {
-      origin: "http://localhost:3000",
+      origin: process.env.FRONTEND_URL,
       credentials: true,
     },
   });
